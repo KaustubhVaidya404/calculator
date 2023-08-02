ca791fdbffd7c161ed399ec19d1e5d99b3666787
Kaustubh <pro.vaidya4413@gmail.com>
2023-08-02 22:49:32 +0530

    debug tag removed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:calculator/ui/home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,
   home: Solve(),
 ));
 
